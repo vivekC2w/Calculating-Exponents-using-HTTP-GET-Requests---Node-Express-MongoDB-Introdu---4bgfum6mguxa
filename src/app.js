4087b34697eae510aef6aa9edd13bd7a5426fdd0
vivekC2w <68582416+vivekC2w@users.noreply.github.com>
2023-10-20 17:10:25 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,17 @@ const server = http.createServer((req, res) => {
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
-      
+      if(value1 <= 0) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("The operation cannot be performed");
+      } else if(!value1 || !value2) {
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.end("The operation cannot be performed");
+      } else {
+        const answer = Math.pow(value1, value2);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("The result is " + answer);
+      }
     });
     }
 });
